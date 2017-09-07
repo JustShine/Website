31456d005036328876cc93527b3b2a6c571a5162
JustShine <catalin.moldovan97@gmail.com>
2017-09-07 15:42:20 +0300

    Mysite Form

--- Mysite/cod.php
+++ Mysite/cod.php
@@ -1,16 +1,18 @@
 <?php
   $db_name="root";
   $db_password="qwerty";
-  $database="sertar";
+  $database="register";
 
   $conn = new mysqli('localhost',$db_name,$db_password,$database);
   if ($conn->connect_error) 
     die('Could not connect: ' . $conn->connect_error);
 
-  $sql = "INSERT INTO dosare
-          VALUES (NULL, '" . $_POST["nume"] . "' , '" . $_POST["pagini"] . "' , '" . $_POST["an"] . "');";
+  $sql = "INSERT INTO accounts
+          VALUES (NULL, '" . $_POST["username"] . "' , '" . $_POST["email"] . "' , '" . $_POST["pass"] . "',
+		  '" . $_POST["birth"] . "', '" . $_POST["gender"] . "', '" . $_POST["terms"] . "');";
 
-  if ($conn->query($sql) === TRUE) 
+	
+  if ($conn->query($sql) === TRUE && count($errors) == 0) 
     echo "New record created successfully";
   else
     echo "Error: " . $sql . "<br>" . $conn->error;
